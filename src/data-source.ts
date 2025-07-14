82bd15f25634162241874be37d9db2c20e0cc26e
Victor Luís <victorluiscaguiar@gmail.com>
2025-07-14 18:04:07 -0300

    feat: initialize data source configuration

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "mysecretpassword",
+  database: process.env.DB_NAME || "mydatabase",
+  synchronize: true, // False in production,
+  entities: [],
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 const app = express();
 
@@ -10,11 +11,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World!" });
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log("Press CTRL+C to stop the server");
-});
+    app.get("/", (req: Request, res: Response) => {
+      res.json({ message: "Hello, World!" });
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+      console.log("Press CTRL+C to stop the server");
+    });
+  })
+  .catch((error) => {
+    console.error("Error during Data Source initialization:", error);
+    process.exit(1);
+  });
